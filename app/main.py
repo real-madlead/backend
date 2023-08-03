847477e37ce255a9b15c70f499ef53f43310664a
ryunosuke121 <ryunosuke.ito@horizon-cg.com>
2023-08-03 16:36:00 +0900

    CORS設定

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.controllers import floor
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(floor.router, prefix="/api/v1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/api/v1")
 def root():
